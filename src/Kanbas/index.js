fe2aa5ef7f1976c8799285472711b2b9c58b2748
kirtanakrishnan <kirtana.k8@gmail.com>
2023-11-28 14:51:39 -0500

    import changes

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,5 +1,5 @@
 import KanbasNavigation from "./KanbasNavigation";
-import {BrowserRouter, Link} from "react-router-dom";
+import {BrowserRouter, Link } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import AccountNavigation from "./Account";
--- src/users/table.js
+++ src/users/table.js
@@ -1,4 +1,5 @@
-import React, { Link, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { BsTrash3Fill, BsPlusCircleFill, BsFillCheckCircleFill, BsPencil }
   from "react-icons/bs";
 import * as client from "./client";
